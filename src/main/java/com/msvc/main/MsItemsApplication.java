63c56d388883b7dbaee02061955db50a62aed226
Diego Valencia <diego.val.maya@gmail.com>
2025-10-13 21:17:23 -0600

    Initial structure

---
+++ src/main/java/com/msvc/main/MsItemsApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsItemsApplication.class, args);
+	}
+
+}
